29017f624edf5a35bfe2d91b25cc3d4237a511af
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2024-02-08 16:25:48 +0100

    Add files via upload

--- iframes/startpic/startpic.js
+++ iframes/startpic/startpic.js
@@ -20,23 +20,30 @@ function draw() {
   background(0);
   image(img, 0, 0,width,height); 
   if(mouseIsPressed){
-    spreadX = width * 0.07;
-    spreadY = height * 0.15;
+    spreadX = width * 0.15;
+    spreadY = height * 0.25;
+    if(frameCount % 3 == 0) {
+      x1 = mouseX+random(-spreadX,spreadX);
+      y1 = mouseY+random(-spreadY,spreadY);
+      size1 = random (10, height *0.4);
+      x2 = mouseX+random(-spreadX,spreadX);
+      y2 = mouseY+random(-spreadY,spreadY);
+      size2 = random (10, height *0.4);
+    }
   }
   else{
-    spreadX = width * 0.5;
-    spreadY = height *0.5;
+      if(frameCount % 3 == 0) {
+      x1 = random(0,width);
+      y1 = random(0,height);
+      size1 = random (10, height *0.4);
+      x2 = random(0,width);
+      y2 = random(0,height);
+      size2 = random (10, height *0.4);
+    }
   }  
   fill(38,123,137);
   noStroke();
-  if(frameCount % 3 == 0) {
-    x1 = mouseX+random(-spreadX,spreadX);
-    y1 = mouseY+random(-spreadY,spreadY);
-    size1 = random (10, height *0.4);
-    x2 = mouseX+random(-spreadX,spreadX);
-    y2 = mouseY+random(-spreadY,spreadY);
-    size2 = random (10, height *0.4);
-  }
+
   rect(x1,y1,size1,size1*0.5);
   rect(x2,y2,size2,size2*0.5);
 }
